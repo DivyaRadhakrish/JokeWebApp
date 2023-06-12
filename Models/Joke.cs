0cc77baf079639024e240c673f9f3a610eda24a7
DivyaRadhakrish <72549399+DivyaRadhakrish@users.noreply.github.com>
2023-06-12 12:47:36 +0200

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,19 @@
+﻿namespace JokeWebApp.Models
+{
+    public class Joke
+    {
+        public int ID { get; set; }
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        public string UserName { get; set; }
+
+
+        public Joke()
+        {
+            
+              
+         }
+    }
+}
